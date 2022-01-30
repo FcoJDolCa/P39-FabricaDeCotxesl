b898b523271bb10bb0e1df867d90a388b4bd7288
FcoJDolCa <98600737+FcoJDolCa@users.noreply.github.com>
2022-01-30 20:14:44 +0100

    Initial commit

---
+++ src/main/java/Coche.java
@@ -0,0 +1,29 @@
+public class Coche {
+
+    private String matricula;
+    private String marca;
+    private String model;
+    private String color;
+    private boolean sostreSolar;
+    private int km;
+    private int numPuertas;
+    private int numPlazas;
+
+    public Coche() {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.color = color;
+        this.sostreSolar = sostreSolar;
+        this.km = km;
+        this.numPuertas = numPuertas;
+        this.numPlazas = numPlazas;
+    }
+    public Coche() {
+
+    }
+
+    public Coche() {
+
+    }
+}
---
+++ src/main/java/Fabrica.java
@@ -0,0 +1,15 @@
+public class Fabrica {
+
+    public static void main(String[] args) {
+        String matricula;
+        String marca;
+        String model;
+        String color;
+        boolean sostreSolar;
+        int km;
+        int numPuertas;
+        int numPlazas;
+
+        Coche coche = new Coche();
+    }
+}
