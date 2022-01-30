a723f8dcf52c051e69c796439d10933b7b4fa7e7
FcoJDolCa <98600737+FcoJDolCa@users.noreply.github.com>
2022-01-30 21:18:54 +0100

    hh

--- src/main/java/Coche.java
+++ src/main/java/Coche.java
@@ -9,7 +9,8 @@ public class Coche {
     private int numPuertas;
     private int numPlazas;
 
-    public Coche() {
+    public Coche(String matricula, String marca, String model, String color,
+                 boolean sostreSolar, int km, int numPuertas, int numPlazas) {
         this.matricula = matricula;
         this.marca = marca;
         this.model = model;
@@ -19,15 +20,69 @@ public class Coche {
         this.numPuertas = numPuertas;
         this.numPlazas = numPlazas;
     }
-    public Coche() {
 
+   /* public Coche() {
+
+    }*/
+
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
     }
 
-    public Coche() {
-
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 
-    public Coche() {
-
+    public String getModel() {
+        return model;
     }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean getSostreSolar() {
+        return sostreSolar;
+    }
+    public void setSostreSolar(boolean sostreSolar) {
+        this.sostreSolar = sostreSolar;
+    }
+
+    public int getKm() {
+        return km;
+    }
+    public void setKm(int km) {
+        this.km = km;
+    }
+
+    public int getNumPuertas() {
+        return numPuertas;
+    }
+    public void setNumPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    public int getNumPlazas() {
+        return numPlazas;
+    }
+    public void setNumPlazas(int numPlazas) {
+        this.numPlazas = numPlazas;
+    }
+
+
+
+
+
 }
--- src/main/java/Fabrica.java
+++ src/main/java/Fabrica.java
@@ -1,15 +1,16 @@
 public class Fabrica {
 
     public static void main(String[] args) {
-        String matricula;
-        String marca;
-        String model;
-        String color;
-        boolean sostreSolar;
-        int km;
-        int numPuertas;
-        int numPlazas;
 
-        Coche coche = new Coche();
+        Coche car1 = new Coche("1234-DF", "Seat", "Toledo",
+                "blau", false, 200, 3, 5);
+        Coche car2 = new Coche("5678-AG", "Fiat", "Uno ",
+                "roig", true, 300, 3, 2 );
+        Coche car3 = new Coche("29012-HH", "BMW", "850",
+                "gris", false, 400, 5, 5);
+        Coche car4 = new Coche("3456-XS", "VW", "Caddy",
+                "blanc", true, 500, 5, 7);
+
+
     }
 }
